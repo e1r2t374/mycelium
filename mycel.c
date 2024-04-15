b3dc04a1e673652055ab24d44eb16d551a91c150
Milo <79590178+e1r2t374@users.noreply.github.com>
2024-04-15 11:00:04 -0500

    Revamp

--- mycel.c
+++ mycel.c
@@ -1,106 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
+#include <unistd.h>
+#include <sys/wait.h>
 #define RED "\033[38;2;230;0;0m"
 #define YELLOW "\033[38;2;200;200;0m"
 #define GREEN "\033[38;2;0;150;0m"
 #define NORMAL "\033[0m"
 #define BOLD "\033[1m"
-void help(){
-	printf("%s%s[Usage]:%s\n\n",BOLD,YELLOW,NORMAL);
-	printf("%s%s(Syntax):%s\n./%s\b\b -e <ENUMERATE> -s <STEALTH>\n\n",BOLD,YELLOW,NORMAL,__FILE__);
-	printf("%s%s(Example):%s\n ./%s\b\b -e 1 -s 1\n\n",BOLD,YELLOW,NORMAL,__FILE__);
-	printf("%s%s(Enumeration Options):\n%s",BOLD,YELLOW,NORMAL);
-	printf("Enumeration 1: Everything.\n");
-	printf("Enumeration 2: Packages.\n");
-	printf("Enumeration 3: Privilege Escalation.\n");
-	printf("Enumeration 4: Sensitive Information.\n");
-	printf("Enumeration 5: Network.\n");
-	printf("\n%s%s(Stealth Options):\n%s",BOLD,YELLOW,NORMAL);
-	printf("Stealth 0: (DEFAULT)No Stealth.\n");
-	printf("Stealth 1: Stealth.\n");
-	printf("%s\n",NORMAL);
+/*
+TODO
+add windows and mac detection
+add options such as steath and option without color
+*/
+void error(const char *err_msg){
+	fprintf(stderr,"\033[1m%sERROR:%s\033[0m\n",RED,err_msg);
+	exit(EXIT_FAILURE);
 }
-void error(char* errmsg){
-	fprintf(stderr,"\033[1m%sERROR:%s\033[0m\n",RED,errmsg);
-	help();
-	abort();
-}
-int main(int argc, char **argv) {
-	if(argc < 2){error("Not Enough Flags Provided!");}
-	else{
-		int opt; int e = 0; int s = 0;
-		while ((opt = getopt(argc, argv, "e:s::")) != -1){
-			switch (opt){
-				case 'e':
-					if(atoi(optarg) > 0 && atoi(optarg) <= 4){
-						e = atoi(optarg);
-					}
-					else{error("Invalid Enumeration Option!");}
-					break;
-				case 's': 
-					if(atoi(optarg) == 0 || atoi(optarg) == 1){
-						s = atoi(optarg);
-					}
-					else{error("Invalid Stealth Option!");}
-					break;
-				case '?': error("Invalid Flag!");break;
-				default:error("You broke something!");break;
-			}
+int sys_cmd(const char *command, char *output) {
+	int pipefd[2];
+    	pid_t pid;
+	int status;
+	if (pipe(pipefd) == -1) {error("Error creating pipe.");}
+	pid = fork();
+	if (pid < 0) {error("Fork failed.");}
+	if (pid == 0) {
+		close(pipefd[0]);
+		if (dup2(pipefd[1], STDOUT_FILENO) == -1) {error("Failed to duplicate file descriptor.");}
+		close(pipefd[1]);
+		if (execl("/bin/sh", "sh", "-c", command, NULL) == -1) {error("Error executing command.");}
+    	} 
+	else {
+		close(pipefd[1]);
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+			ssize_t n = read(pipefd[0], output, 4096);
+			if (n > 0) {output[n] = '\0';}
+			close(pipefd[0]);
+			return 0;
 		}
-		if(e == 0){error("Invalid Flag!");}
-		else{
-			switch(e){
-				case 1:
-					switch(s){
-						case 0:break;
-						case 1:break;
-						case'?':break;
-						default:break;
-					}
-					break;
-				
-				case 2:
-					switch(s){
-						case 0:
-							if(system("dpkg --get-selections | wc -l 1>/dev/null 2>/dev/null") == 0){ 
-								printf("%s%s[Apt installed]%s\n",BOLD,GREEN,NORMAL);
-								if(system("echo \"Packages:\"&& dpkg --get-selections | wc -l 2>/dev/null") != 0){
-									error("dpkg package error");
-								}
-								if(system("echo \"Versions:\"&& dpkg -p 2>/dev/null") != 0){
-									error("dpkg version error");
-								}
-							}
-							break;
-						case 1:break;
-						case'?':break;
-						default:break;
-					}
-					break;
-				case 3:
-					switch(s){
-						case 0:break;
-						case 1:break;
-						case'?':break;
-						default:break;
-					}
-					break;
-				case 4:
-					switch(s){
-						case 0:break;
-						case 1:break;
-						case'?':break;
-						default:break;
-					}
-					break;
-				case'?':break;
-				default:break;
-			}
-			
-			printf("\ne:%d,s:%d\n",e,s);
+		else {
+			close(pipefd[0]);
+			return 1;
 		}
 	}
-  	return 0;
+	return -1;
+}
+
+int main(void) {
+	char *commands[] = {
+	"cat /etc/*-release 2>/dev/null", //OS
+	"echo ID=$(id) && for i in $(cut -d':' -f1 /etc/passwd);do id $i;done 2>/dev/null", //IDs and Groups
+	"echo 'test' 2>/dev/null" //Functionality test
+	};
+	int size = sizeof(commands) / sizeof(commands[0]);
+	for (int i = 0; i < size; i++) {
+		char output[4096] = {0};
+		if (sys_cmd(commands[i], output) == 0) {
+			switch (i) {
+				case 0:
+					printf("%s\n[Operating System Info]:\033[0m\n",GREEN);
+					printf("%s\n%s\n",output,NORMAL);
+					break;
+				case 1:
+					printf("%s\n[IDs and Groups]:\033[0m\n",GREEN);
+					printf("%s\n%s\n",output,NORMAL);
+					break;
+				default:printf("%s\n[No Title Available]:\033[0m\n",GREEN);break;
+			}
+		} 
+		else {
+			switch (i) {
+				case 0:printf("%s\n[Operating System Info]:\033[0m\n",RED);break;
+				case 1:printf("%s\n[IDs and Groups]:\033[0m\n",RED);break;
+				default:printf("%s\n[No Title Available]:\033[0m\n",RED);break;
+			}
+		}
+	}
+	return 0;
 }
